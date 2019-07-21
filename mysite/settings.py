e7a99f580e3fbab1f6e95171d838fb8ca860d0b9
Alexander <ezhkovskii@gmail.com>
2019-07-21 13:02:24 +0500

    Добавлена субд postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoblog',
+        'USER': 'postgres',
+        'PASSWORD': 'alex12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
